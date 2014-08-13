fbaef540cc1ba8e42e97e22a823c8701e8345c0e
H. Marcelo Muriel <marcelomuriel@gmail.com>
2014-08-13 15:00:41 +0200

    dist export-dist.sh file created

---
+++ export-dist.sh
@@ -0,0 +1,19 @@
+wpmob_dir=~/workspace_sc/WPMobileApps/app
+wpmob_theme_dir=$wpmob_dir/themes/mobilissimo
+dist_dir=~/workspace_sc/WPMobileApps/dist/WPMobileApps
+dist_theme_dir=$dist_dir/themes/mobilissimo
+if [ -f $dist_dir ]
+then
+	rm -r  $dist_dir
+fi
+cd $wpmob_dir
+mkdir -p $dist_dir
+git archive master | tar -x -C $dist_dir
+cd $wpmob_theme_dir
+mkdir -p $dist_theme_dir
+git archive master | tar -x -C $dist_theme_dir
+cd $dist_dir
+rm export-dist.sh
+rm create-po.sh
+cd ..
+tar -czf WPMobileApps.tar.gz WPMobileApps
